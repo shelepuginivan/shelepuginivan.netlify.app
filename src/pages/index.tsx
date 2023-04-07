862d4333aa8ead13c0c0d57fc82926ec8f4a8656
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-08 00:30:35 +0300

    Added navigation with content cards

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,22 @@
-import Head from 'next/head'
+import blog from '@/assets/blog.jpg'
+import contacts from '@/assets/contacts.jpg'
+import gallery from '@/assets/gallery.jpg'
+import projects from '@/assets/projects.jpg'
+import ContentCard from '@/components/ContentCard/ContentCard'
+import Container from '@/ui/Container/Container'
+import ResponsiveGrid from '@/ui/ResponsiveGrid/ResponsiveGrid'
 
 export default function Home() {
 	return (
-		<>
-			<Head>
-				<title>Create Next App</title>
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-			</Head>
-			<main></main>
-		</>
+		<main>
+			<Container>
+				<ResponsiveGrid>
+					<ContentCard href='/projects' title='Проекты' backgroundImage={projects} />
+					<ContentCard href='/blog' title='Блог' backgroundImage={blog} />
+					<ContentCard href='/gallery' title='Галерея' backgroundImage={gallery} />
+					<ContentCard href='/contacts' title='Контакты' backgroundImage={contacts} />
+				</ResponsiveGrid>
+			</Container>
+		</main>
 	)
 }
