11dac5405292d0e36dafa4d6b2dc4bf773c97179
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-08 20:37:37 +0300

    Added AboutMe component to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import blog from '@/assets/blog.jpg'
 import contacts from '@/assets/contacts.jpg'
 import gallery from '@/assets/gallery.jpg'
 import projects from '@/assets/projects.jpg'
+import AboutMe from '@/components/AboutMe/AboutMe'
 import ContentCard from '@/components/ContentCard/ContentCard'
 import Container from '@/ui/Container/Container'
 import ResponsiveGrid from '@/ui/ResponsiveGrid/ResponsiveGrid'
@@ -9,6 +10,7 @@ import ResponsiveGrid from '@/ui/ResponsiveGrid/ResponsiveGrid'
 export default function Home() {
 	return (
 		<main>
+			<AboutMe/>
 			<Container>
 				<ResponsiveGrid>
 					<ContentCard href='/projects' title='Проекты' backgroundImage={projects} />
