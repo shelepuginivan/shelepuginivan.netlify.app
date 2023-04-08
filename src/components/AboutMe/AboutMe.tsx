775d159a8363cdddce1ac62b5d52e751b3beb86b
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-08 20:37:15 +0300

    Added AboutMe component

---
+++ src/components/AboutMe/AboutMe.tsx
@@ -0,0 +1,34 @@
+import {Nunito, Roboto_Flex} from 'next/font/google'
+import {FC} from 'react'
+
+import ivan from '@/assets/ivan.png'
+
+import styles from './aboutMe.module.sass'
+
+const nunito = Nunito({
+	subsets: ['cyrillic'],
+	weight: '400'
+})
+
+const robotoFlex = Roboto_Flex({
+	subsets: ['cyrillic'],
+	weight: '400'
+})
+
+const AboutMe: FC = () => {
+	return (
+		<section className={styles.about}>
+			<div className={styles.text}>
+				<h2 className={nunito.className}>Обо мне</h2>
+				<p className={robotoFlex.className}>
+					Увлекаюсь информатикой, программированием, веб-разработкой.
+					В свободное время пишу статьи на самые разные темы.
+					Участвую в проектных конкурсах и олимпиадах по разным предметам.
+				</p>
+			</div>
+			<img src={ivan.src} alt=""/>
+		</section>
+	)
+}
+
+export default AboutMe
