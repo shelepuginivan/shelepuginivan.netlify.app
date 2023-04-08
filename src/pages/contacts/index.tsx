88a8035c7f8624e9288c0b479b03a2a6818e6b72
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-09 01:17:06 +0300

    Added Social component in contacts page

--- src/pages/contacts/index.tsx
+++ src/pages/contacts/index.tsx
@@ -1,8 +1,10 @@
 import FeedbackForm from '@/components/FeedbackForm/FeedbackForm'
+import Social from '@/components/Social/Social'
 
 const Contacts = () => {
 	return (
 		<section>
+			<Social/>
 			<FeedbackForm/>
 		</section>
 	)
