19c11caec54a85cb813be359b8d12dbb508e15d4
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-09 01:16:52 +0300

    Added Social component

---
+++ src/components/Social/Social.tsx
@@ -0,0 +1,48 @@
+import {Nunito, Roboto_Flex} from 'next/font/google'
+import {FC} from 'react'
+
+import phone from '@/assets/phone.png'
+
+import styles from './social.module.sass'
+
+const nunito = Nunito({
+	subsets: ['cyrillic'],
+	weight: '300'
+})
+
+const robotoFlex = Roboto_Flex({
+	subsets: ['latin'],
+	weight: '400'
+})
+
+const Social: FC = () => {
+	return (
+		<div className={styles.social}>
+			<div>
+				<h1 className={nunito.className}>Контакты</h1>
+
+				<ul className={robotoFlex.className}>
+					<li>
+						<i className='icon-contacts'> </i>
+						<a href="mailto:shelepuginivanm@gmail.com">shelepuginivanm@gmail.com</a>
+					</li>
+					<li>
+						<i className='icon-telegram'> </i>
+						<a href="https://t.me/shelepugin_ivan">https://t.me/shelepugin_ivan</a>
+					</li>
+					<li>
+						<i className='icon-vk'> </i>
+						<a href="https://vk.com/shelepuginivan">https://vk.com/shelepuginivan</a>
+					</li>
+					<li>
+						<i className='icon-github'> </i>
+						<a href="https://github.com/shelepuginivan">https://github.com/shelepuginivan</a>
+					</li>
+				</ul>
+			</div>
+			<img src={phone.src} alt=""/>
+		</div>
+	)
+}
+
+export default Social
