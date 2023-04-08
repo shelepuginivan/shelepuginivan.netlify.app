668ba819da7e6c774505b709096f843ae7cd7c93
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-08 03:14:59 +0300

    Added Header in _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,9 +6,11 @@ import '@/assets/fonts/icomoon/style.css'
 import type { AppProps } from 'next/app'
 
 import Footer from '@/components/Footer/Footer'
+import Header from '@/components/Header/Header'
 
 export default function App({ Component, pageProps }: AppProps) {
 	return <>
+		<Header/>
 		<Component {...pageProps} />
 		<Footer/>
 	</> 
