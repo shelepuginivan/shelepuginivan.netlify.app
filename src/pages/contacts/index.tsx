3ef56fa1f3bdf396b9c2ab98e7a2fb30b904b0e8
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-08 23:07:30 +0300

    Added contacts page with feedback form

---
+++ src/pages/contacts/index.tsx
@@ -0,0 +1,11 @@
+import FeedbackForm from '@/components/FeedbackForm/FeedbackForm'
+
+const Contacts = () => {
+	return (
+		<section>
+			<FeedbackForm/>
+		</section>
+	)
+}
+
+export default Contacts
