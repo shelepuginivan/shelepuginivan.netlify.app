d603494a2eeeb0ffdd772b3c951c95018656d3d5
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-09 16:31:44 +0300

    Added GalleryCategoriesList component to the gallery page

--- src/pages/gallery/index.tsx
+++ src/pages/gallery/index.tsx
@@ -1,7 +1,23 @@
+import {Nunito} from 'next/font/google'
 import {FC} from 'react'
 
+import GalleryCategoriesList from '@/components/GalleryCategoriesList/GalleryCategoriesList'
+import Container from '@/ui/Container/Container'
+
+const nunito = Nunito({
+	subsets: ['cyrillic'],
+	weight: '300'
+})
+
 const Gallery: FC = () => {
-	return
+	return (
+		<main>
+			<Container>
+				<h1 className={nunito.className}>Галерея</h1>
+				<GalleryCategoriesList/>
+			</Container>
+		</main>
+	)
 }
 
 export default Gallery
