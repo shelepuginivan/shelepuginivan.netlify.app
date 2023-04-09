2bdb0b4226f509064afc0e0b4a8e99126fea7c68
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-10 01:18:46 +0300

    Added ArticleList to /blog page

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,7 +1,23 @@
+import {Nunito} from 'next/font/google'
 import {FC} from 'react'
 
+import ArticleList from '@/components/ArticleList/ArticleList'
+import Container from '@/ui/Container/Container'
+
+const nunito = Nunito({
+	subsets: ['cyrillic'],
+	weight: '300'
+})
+
 const Blog: FC = () => {
-	return <main></main>
+	return (
+		<main>
+			<Container>
+				<h1 className={nunito.className}>Статьи</h1>
+				<ArticleList/>
+			</Container>
+		</main>
+	)
 }
 
 export default Blog
