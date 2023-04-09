64c4dc01438c10f5d2b7312d4318582ab9d679cf
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-09 16:30:54 +0300

    Changed api response format, now return GalleryCategory array

--- src/pages/api/gallery/index.ts
+++ src/pages/api/gallery/index.ts
@@ -1,10 +1,24 @@
 import {NextApiRequest, NextApiResponse} from 'next'
 
 import {GalleryService} from '@/utils/GalleryService'
+import {GalleryCategory} from '@/utils/types/GalleryCategory'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 	const categories = await GalleryService.getCategories()
-	res.status(200).json(categories)
+	const categoriesWithPreview: GalleryCategory[] = []
+
+	for (let i = 0; i < categories.length; i++) {
+		const name = categories[i]
+
+		const category: GalleryCategory = {
+			previewUrl: await GalleryService.getRandomImageUrlByCategory(name),
+			name
+		}
+
+		categoriesWithPreview.push(category)
+	}
+
+	res.status(200).json(categoriesWithPreview)
 }
 
 export default handler
