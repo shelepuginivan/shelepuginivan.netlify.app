168e0e8dd24d953d91963bc343a5199a993b6664
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-09 15:24:12 +0300

    Added method that returns `GalleryItem` array by category

--- src/utils/GalleryService.ts
+++ src/utils/GalleryService.ts
@@ -1,5 +1,7 @@
 import {MongoClient} from 'mongodb'
 
+import {GalleryItem} from '@/utils/types/GalleryItem'
+
 export class GalleryService {
 	static async getCategories(): Promise<string[]> {
 		const client = new MongoClient(process.env.MONGO_URI)
@@ -9,6 +11,21 @@ export class GalleryService {
 
 		try {
 			return await database.collection('gallery').distinct('category')
+		} finally {
+			await client.close()
+		}
+	}
+
+	static async getGalleryItemsByCategory(category: string): Promise<GalleryItem[]> {
+		const client = new MongoClient(process.env.MONGO_URI)
+		await client.connect()
+
+		const database = client.db(process.env.MONGO_DB_NAME)
+
+		try {
+			const galleryItems = await database.collection('gallery').find({category})
+
+			return await galleryItems.toArray() as GalleryItem[]
 		} finally {
 			await client.close()
 		}
