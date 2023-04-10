cc496f032e28c4d73e9cd2b18f1156cfc731017e
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-10 03:07:37 +0300

    Moved database variable declaration inside try block

--- src/utils/GalleryService.ts
+++ src/utils/GalleryService.ts
@@ -22,9 +22,8 @@ export class GalleryService {
 		const client = new MongoClient(process.env.MONGO_URI as string)
 		await client.connect()
 
-		const database = client.db(process.env.MONGO_DB_NAME as string)
-
 		try {
+			const database = client.db(process.env.MONGO_DB_NAME as string)
 			const galleryItems = await database.collection('gallery').find({category})
 
 			if (!galleryItems) {
