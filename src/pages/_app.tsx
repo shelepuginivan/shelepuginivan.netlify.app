ea86240f320fdf5de75abc653972426ecef45f14
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-10 21:14:29 +0300

    Added media queries, made styles adaptive

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.sass'
 import '@/styles/reset.sass'
 import '@/styles/variables.sass'
+import '@/styles/media.sass'
 import '@/assets/fonts/icomoon/style.css'
 
 import type { AppProps } from 'next/app'
