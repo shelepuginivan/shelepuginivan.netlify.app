99fc634fa29bac598f621df345d7226b4150181d
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-10 03:05:44 +0300

    Added env variables check

--- src/utils/ProjectsService.ts
+++ src/utils/ProjectsService.ts
@@ -1,14 +1,19 @@
 import {MongoClient} from 'mongodb'
 
+import {ServerExceptionFactory} from '@/utils/ServerExceptionFactory'
 import {Project} from '@/utils/types/Project'
 
 export class ProjectService {
 	static async getAllProjects(): Promise<Project[]> {
-		const client = new MongoClient(process.env.MONGO_URI as string)
+		if (!process.env.MONGO_URI || !process.env.MONGO_DB_NAME) {
+			throw ServerExceptionFactory.internalServerError('Внутренняя ошибка сервера')
+		}
+
+		const client = new MongoClient(process.env.MONGO_URI)
 		await client.connect()
 
 		try {
-			const database = client.db(process.env.MONGO_DB_NAME as string)
+			const database = client.db(process.env.MONGO_DB_NAME)
 			const projects = await database.collection('project').find()
 			return await projects.toArray() as unknown as Project[]
 		} finally {
