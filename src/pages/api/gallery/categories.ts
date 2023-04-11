045ddfe1dffefc3835f22579d225a2ece2e89c3f
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-11 20:00:39 +0300

    Added `/api/gallery/categories` API endpoint that returns array of slugs (for sitemap.xml)

---
+++ src/pages/api/gallery/categories.ts
@@ -0,0 +1,24 @@
+import {NextApiRequest, NextApiResponse} from 'next'
+
+import {GalleryService} from '@/utils/GalleryService'
+import {ServerException} from '@/utils/ServerException'
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+	try {
+		const allCategories = await GalleryService.getCategories()
+
+		res.status(200).json(allCategories)
+	} catch (e) {
+		if (e instanceof ServerException) {
+			return res.status(e.status).json({message: e.message})
+		}
+
+		if (e instanceof Error) {
+			return res.status(500).json({message: e.message})
+		}
+
+		res.status(500).json({message: 'Внутренняя ошибка сервера'})
+	}
+}
+
+export default handler
