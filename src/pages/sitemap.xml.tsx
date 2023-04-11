0091ac1bf5dfb69952d644751b9efa37a74314bf
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-11 20:00:56 +0300

    Used optimized API endpoints

--- src/pages/sitemap.xml.tsx
+++ src/pages/sitemap.xml.tsx
@@ -10,26 +10,26 @@ const sitemapLocation = (url: string) => {
 }
 
 export const getServerSideProps: GetServerSideProps = async ({res}) => {
-	const articlesResponse = await fetch(`${getHost()}/api/blog`)
-	const articlesJson = await articlesResponse.json()
+	const articlesResponse = await fetch(`${getHost()}/api/blog/slugs`)
+	const slugsJson = await articlesResponse.json() as unknown
 
-	const categoriesResponse = await fetch(`${getHost()}/api/gallery`)
-	const categoriesJson = await categoriesResponse.json()
+	const categoriesResponse = await fetch(`${getHost()}/api/gallery/categories`)
+	const categoriesJson = await categoriesResponse.json() as unknown
 
 	const articles = (
-		Array.isArray(articlesJson) &&
-		articlesJson.length > 0 &&
-		Object.hasOwnProperty.call(articlesJson[0], 'slug')
+		Array.isArray(slugsJson) &&
+		slugsJson.length > 0 &&
+		slugsJson.every(item => typeof item === 'string')
 	)
-		? articlesJson.map(article => `${getHost()}/blog/${article.slug}`)
+		? slugsJson.map(slug => `${getHost()}/blog/${slug}`)
 		: []
 
 	const galleryCategories = (
 		Array.isArray(categoriesJson) &&
 		categoriesJson.length > 0 &&
-		Object.hasOwnProperty.call(categoriesJson[0], 'name')
+		categoriesJson.every(item => typeof item === 'string')
 	)
-		? categoriesJson.map(category => `${getHost()}/gallery/${category.name}`)
+		? categoriesJson.map(category => `${getHost()}/gallery/${category}`)
 		: []
 
 	const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
