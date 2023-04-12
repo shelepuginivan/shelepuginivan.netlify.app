11c0810c50da1d300f067c2c064a319c0b5a8db2
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-12 17:55:45 +0300

    Added prop `header`

--- src/ui/ErrorMessage/ErrorMessage.tsx
+++ src/ui/ErrorMessage/ErrorMessage.tsx
@@ -3,6 +3,11 @@ import {FC} from 'react'
 
 import styles from './errorMessage.module.sass'
 
+type PropsType = {
+	header?: string
+	message: string
+}
+
 const nunito = Nunito({
 	subsets: ['cyrillic'],
 	weight: '300'
@@ -13,9 +18,9 @@ const robotoFlex = Roboto_Flex({
 	weight: '400'
 })
 
-const ErrorMessage: FC<{message: string}> = ({message}) =>
+const ErrorMessage: FC<PropsType> = ({message, header}) =>
 	<div className={styles.errorMessage}>
-		<h1 className={nunito.className}>Произошла ошибка!</h1>
+		<h1 className={nunito.className}>{header ?? 'Произошла ошибка!'}</h1>
 		<p className={robotoFlex.className}>{message}</p>
 		<a href='/'><i className='icon-home'></i></a>
 	</div>
