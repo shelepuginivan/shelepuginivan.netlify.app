830df4ed9807fa96e8b7159caf7114fdbd1bd6ac
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-12 19:45:15 +0300

    Added links to favicon and manifest.json

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,9 +4,13 @@ export default function Document() {
 	return (
 		<Html lang='ru'>
 			<Head>
+				<meta name='charset' content='UTF-8'/>
 				<meta name='author' content='Иван Шелепугин'/>
 				<meta name='copyright' content='Иван Шелепугин'/>
-				<meta name='charset' content='UTF-8'/>
+				<link rel="manifest" href="/manifest.json"/>
+				<link rel="icon" href="/favicon.png"/>
+				<link rel="shortcut icon" href="/favicon.png"/>
+				<link rel="favicon" href="/favicon.png"/>
 			</Head>
 			<body>
 				<Main />
