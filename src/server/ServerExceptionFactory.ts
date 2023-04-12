c736e5e38b34face26c21298ea236ae4ec11c7b2
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-12 17:53:42 +0300

    Added notFound method

--- src/server/ServerExceptionFactory.ts
+++ src/server/ServerExceptionFactory.ts
@@ -5,6 +5,10 @@ export class ServerExceptionFactory {
 		return new ServerException(400, message)
 	}
 
+	static notFound(message: string): ServerException {
+		return new ServerException(404, message)
+	}
+
 	static internalServerError(message: string): ServerException {
 		return new ServerException(500, message)
 	}
