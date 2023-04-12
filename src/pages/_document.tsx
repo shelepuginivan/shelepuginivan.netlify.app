c472aacf69bfc5a675983f8c45816ba1304e909c
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-12 17:54:45 +0300

    Fixed code styles

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,11 +2,11 @@ import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
 	return (
-		<Html lang="ru">
+		<Html lang='ru'>
 			<Head>
 				<meta name='author' content='Иван Шелепугин'/>
 				<meta name='copyright' content='Иван Шелепугин'/>
-				<meta name='charset' content='UTF-8' />
+				<meta name='charset' content='UTF-8'/>
 			</Head>
 			<body>
 				<Main />
