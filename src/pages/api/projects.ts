9b5dbba8712d89b3cc21db475d478be0f4c70b2b
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-27 01:39:28 +0300

    Get pagination parameters from query

--- src/pages/api/projects.ts
+++ src/pages/api/projects.ts
@@ -4,8 +4,21 @@ import {ProjectService} from '@/server/ProjectsService'
 import {ServerException} from '@/server/ServerException'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+	let {page, projectsPerPage} = req.query
+
+	if (typeof page !== 'string' || isNaN(Number(page))) {
+		page = '1'
+	}
+
+	if (typeof projectsPerPage !== 'string' || isNaN(Number(projectsPerPage))) {
+		projectsPerPage = '10'
+	}
+
 	try {
-		const projects = await ProjectService.getAllProjects()
+		const projects = await ProjectService.getAllProjects(
+			Number(page),
+			Number(projectsPerPage)
+		)
 		res.status(200).json(projects)
 	} catch (e) {
 		if (e instanceof ServerException) {
