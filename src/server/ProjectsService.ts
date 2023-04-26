c79ea43a18e3308ff0dc1aca02e40f05d980dab1
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-27 01:39:09 +0300

    Added pagination

--- src/server/ProjectsService.ts
+++ src/server/ProjectsService.ts
@@ -4,7 +4,7 @@ import {ServerExceptionFactory} from '@/server/ServerExceptionFactory'
 import {Project} from '@/utils/types/Project'
 
 export class ProjectService {
-	static async getAllProjects(): Promise<Project[]> {
+	static async getAllProjects(page: number, projectsPerPage: number): Promise<Project[]> {
 		if (!process.env.MONGO_URI || !process.env.MONGO_DB_NAME) {
 			throw ServerExceptionFactory.internalServerError('Внутренняя ошибка сервера')
 		}
@@ -14,8 +14,15 @@ export class ProjectService {
 
 		try {
 			const database = client.db(process.env.MONGO_DB_NAME)
-			const projects = await database.collection('project').find()
-			return await projects.toArray() as unknown as Project[]
+			const collection = database.collection('project')
+
+			const projects = await collection
+				.find()
+				.skip(projectsPerPage * (page - 1))
+				.limit(projectsPerPage)
+				.toArray()
+
+			return projects as unknown as Project[]
 		} finally {
 			await client.close()
 		}
