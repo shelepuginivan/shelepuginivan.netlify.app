a88ca3abaa018fabbe463c40dda1003e5d0d69d5
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-26 14:39:42 +0300

    Added infinite scroll

--- src/components/CategoryImages/CategoryImages.tsx
+++ src/components/CategoryImages/CategoryImages.tsx
@@ -1,4 +1,5 @@
 import {FC, useEffect, useState} from 'react'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 import Center from '@/ui/Center/Center'
 import ErrorMessage from '@/ui/ErrorMessage/ErrorMessage'
@@ -7,8 +8,26 @@ import Loader from '@/ui/Loader/Loader'
 import styles from './categoryImages.module.sass'
 
 const CategoryImages: FC<{category?: string | string[]}> = ({category}) => {
+	const [currentPage, setCurrentPage] = useState<number>(2)
+	const [hasMore, setHasMore] = useState<boolean>(true)
 	const [images, setImages] = useState<string[] | null>(null)
 	const [errorMessage, setErrorMessage] = useState<string | null>(null)
+
+	const fetchImages = async () => {
+		const res = await fetch(`/api/gallery/${category}?page=${currentPage}`)
+		const json = await res.json()
+
+		if (res.status >= 400) {
+			return setErrorMessage(json.message)
+		}
+
+		if (Array.isArray(json) && json.length === 0) {
+			return setHasMore(false)
+		}
+
+		setImages(prev => [...prev, ...json.map(item => item.url) as string[]])
+		setCurrentPage(prev => prev + 1)
+	}
 
 	useEffect(() => {
 		const fetchGalleryItems = async () => {
@@ -18,7 +37,11 @@ const CategoryImages: FC<{category?: string | string[]}> = ({category}) => {
 			const json: Record<'url', string>[] & Record<'message', string> = await res.json()
 
 			if (res.status >= 400) {
-				setErrorMessage(json.message)
+				return setErrorMessage(json.message)
+			}
+
+			if (Array.isArray(json) && json.length === 0) {
+				return setHasMore(false)
 			}
 
 			setImages(json.map(item => item.url))
@@ -32,20 +55,30 @@ const CategoryImages: FC<{category?: string | string[]}> = ({category}) => {
 			<Loader/>
 		</Center>
 
-	if (errorMessage)
+	if (errorMessage) {
 		return <ErrorMessage message={errorMessage}/>
+	}
 
-	if ((typeof category !== 'string') || images && images.length === 0)
+	if ((typeof category !== 'string') || images && images.length === 0) {
 		return <ErrorMessage message={`Категория "${category}" не найдена`}/>
+	}
 
 	return (
-		<div className={styles.wrapper}>
+		<InfiniteScroll
+			className={styles.wrapper}
+			next={fetchImages}
+			hasMore={hasMore}
+			loader={<div className={styles.loaderWrapper}>
+				<Loader/>
+			</div>}
+			dataLength={images.length}
+		>
 			{
 				images.map(
 					(image, index) => <img src={image} alt="" key={index}/>
 				)
 			}
-		</div>
+		</InfiniteScroll>
 	)
 }
 
