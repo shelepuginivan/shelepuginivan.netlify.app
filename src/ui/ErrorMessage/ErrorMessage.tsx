00f3e99da028ec7f8ad711cedbe6563abdf0e7f6
shelepuginivan <shelepuginivanm@gmail.com>
2023-05-04 18:18:07 +0300

    Link optimization

--- src/ui/ErrorMessage/ErrorMessage.tsx
+++ src/ui/ErrorMessage/ErrorMessage.tsx
@@ -1,4 +1,5 @@
 import {Nunito, Roboto_Flex} from 'next/font/google'
+import Link from 'next/link'
 import {FC} from 'react'
 
 import styles from './errorMessage.module.sass'
@@ -22,7 +23,7 @@ const ErrorMessage: FC<PropsType> = ({message, header}) =>
 	<div className={styles.errorMessage}>
 		<h1 className={nunito.className}>{header ?? 'Произошла ошибка!'}</h1>
 		<p className={robotoFlex.className}>{message}</p>
-		<a href='/'><i className='icon-home'></i></a>
+		<Link href='/'><i className='icon-home'></i></Link>
 	</div>
 
 export default ErrorMessage
