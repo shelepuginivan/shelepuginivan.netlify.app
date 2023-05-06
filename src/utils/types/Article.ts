a4c262d6d91b42c71c9e75556bbad472a959fa79
shelepuginivan <shelepuginivanm@gmail.com>
2023-05-06 18:08:09 +0300

    Fix errors in isArticleData type guard

--- src/utils/types/Article.ts
+++ src/utils/types/Article.ts
@@ -9,9 +9,9 @@ export type Article = {
 export const isArticleData = (article: unknown): article is Omit<Article, 'text'> => {
 	return (
 		typeof article === 'object' &&
-		typeof (article as Record<Article, unknown>).title === 'string' &&
-		typeof (article as Record<Article, unknown>).previewUrl === 'string' &&
-		typeof (article as Record<Article, unknown>).slug === 'string' &&
-		typeof (article as Record<Article, unknown>).publicationTime === 'number'
+		typeof (article as Record<'title', unknown>).title === 'string' &&
+		typeof (article as Record<'previewUrl', unknown>).previewUrl === 'string' &&
+		typeof (article as Record<'slug', unknown>).slug === 'string' &&
+		typeof (article as Record<'publicationTime', unknown>).publicationTime === 'number'
 	)
 }
