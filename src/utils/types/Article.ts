cb542eb20567d727ad77670bb299a89eac9c081c
shelepuginivan <shelepuginivanm@gmail.com>
2023-05-06 18:03:07 +0300

    Add type guard for article data

--- src/utils/types/Article.ts
+++ src/utils/types/Article.ts
@@ -5,3 +5,13 @@ export type Article = {
 	slug: string
 	publicationTime: number
 }
+
+export const isArticleData = (article: unknown): article is Omit<Article, 'text'> => {
+	return (
+		typeof article === 'object' &&
+		typeof (article as Record<Article, unknown>).title === 'string' &&
+		typeof (article as Record<Article, unknown>).previewUrl === 'string' &&
+		typeof (article as Record<Article, unknown>).slug === 'string' &&
+		typeof (article as Record<Article, unknown>).publicationTime === 'number'
+	)
+}
