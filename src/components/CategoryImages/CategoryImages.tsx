0bda7087dc07a51b062d0bbc92345b0b71733501
shelepuginivan <shelepuginivanm@gmail.com>
2023-06-26 19:38:05 +0300

    feat: use hook `useGalleryImagesInfiniteQuery` to fetch images with pagination

--- src/components/CategoryImages/CategoryImages.tsx
+++ src/components/CategoryImages/CategoryImages.tsx
@@ -1,52 +1,22 @@
-import {FC, useCallback, useEffect, useState} from 'react'
+import {FC} from 'react'
 import InfiniteScroll from 'react-infinite-scroll-component'
 
+import {useGalleryImagesInfiniteQuery} from '@/hooks/useGalleryImagesInfiniteQuery'
 import Center from '@/ui/Center/Center'
 import ErrorMessage from '@/ui/ErrorMessage/ErrorMessage'
 import Loader from '@/ui/Loader/Loader'
+import {errorMessage} from '@/utils/errorMessage'
 
 import styles from './categoryImages.module.sass'
 
-const CategoryImages: FC<{category?: string | string[]}> = ({category}) => {
-	const [currentPage, setCurrentPage] = useState<number>(1)
-	const [hasMore, setHasMore] = useState<boolean>(true)
-	const [images, setImages] = useState<string[] | null>(null)
-	const [errorMessage, setErrorMessage] = useState<string | null>(null)
+const CategoryImages: FC<{category: string}> = ({category}) => {
+	const {data, error, fetchNextPage, hasNextPage, isLoading} = useGalleryImagesInfiniteQuery(category)
 
-	const fetchImages = useCallback(async () => {
-		if (!category) return
-
-		const res = await fetch(`/api/gallery/${category}?page=${currentPage}`)
-		const json = await res.json()
-
-		if (res.status >= 400) {
-			return setErrorMessage(json.message)
-		}
-
-		if (images && images.length === 0 && json.length === 0) {
-			return setErrorMessage(`Категория ${category} не найдена`)
-		}
-
-		if (json.length === 0) {
-			return setHasMore(false)
-		}
-
-		setImages(prev => prev
-			? [...prev, ...(json as Record<'url', string>[]).map(item => item.url)]
-			: (json as Record<'url', string>[]).map(item => item.url)
-		)
-		setCurrentPage(prev => prev + 1)
-	}, [category, currentPage])
-
-	useEffect(() => {
-		fetchImages()
-	}, [])
-
-	if (errorMessage) {
-		return <ErrorMessage message={errorMessage}/>
+	if (error) {
+		return <ErrorMessage message={errorMessage(error)}/>
 	}
 
-	if (!images) {
+	if (isLoading) {
 		return <Center>
 			<Loader/>
 		</Center>
@@ -55,18 +25,18 @@ const CategoryImages: FC<{category?: string | string[]}> = ({category}) => {
 	return (
 		<InfiniteScroll
 			className={styles.wrapper}
-			next={fetchImages}
-			hasMore={hasMore}
+			next={fetchNextPage}
+			hasMore={Boolean(hasNextPage)}
 			loader={<div className={styles.loaderWrapper}>
 				<Loader/>
 			</div>}
-			dataLength={images.length}
+			dataLength={data?.pages.length ?? 0}
 		>
-			{
-				images.map(
-					(image, index) => <img src={image} alt='' key={index}/>
+			{data?.pages.map(images =>
+				images.map(image =>
+					<img src={image} alt='' key={image}/>
 				)
-			}
+			)}
 		</InfiniteScroll>
 	)
 }
