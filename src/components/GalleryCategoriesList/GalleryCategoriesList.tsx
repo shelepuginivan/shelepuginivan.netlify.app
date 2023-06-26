9abe50a297f411d896d6f1e5156d5434a13bacd0
shelepuginivan <shelepuginivanm@gmail.com>
2023-06-26 19:36:51 +0300

    feat: use hook `useGalleryCategoriesQuery` to fetch all gallery categories

--- src/components/GalleryCategoriesList/GalleryCategoriesList.tsx
+++ src/components/GalleryCategoriesList/GalleryCategoriesList.tsx
@@ -1,37 +1,21 @@
 import {FC, useEffect, useRef, useState} from 'react'
 
 import ContentCard from '@/components/ContentCard/ContentCard'
+import {useGalleryCategoriesQuery} from '@/hooks/useGalleryCategoriesQuery'
 import Center from '@/ui/Center/Center'
 import ErrorMessage from '@/ui/ErrorMessage/ErrorMessage'
 import Loader from '@/ui/Loader/Loader'
-import {GalleryCategory} from '@/utils/types/GalleryCategory'
+import {errorMessage} from '@/utils/errorMessage'
 
 import styles from './galleryCategoriesList.module.sass'
 
 const GalleryCategoriesList: FC = () => {
-	const [categories, setCategories] = useState<GalleryCategory[] | null>(null)
 	const [showSecret, setShowSecret] = useState<boolean>(false)
-	const [errorMessage, setErrorMessage] = useState<string | null>(null)
-
-	const secretCode = useRef<string>('')
-
-	useEffect(() => {
-		const fetchCategories = async () => {
-			const res = await fetch('/api/gallery')
-			const json = await res.json()
-
-			if (res.status >= 400) {
-				return setErrorMessage(json.message)
-			}
-
-			return setCategories(json)
-		}
-		
-		fetchCategories()
-	}, [])
+	const {data: categories, error, isLoading} = useGalleryCategoriesQuery()
+	const secretCode = useRef('')
 	
-	const secretCodeEnter = (e: KeyboardEvent) => {
-		switch (e.key) {
+	const secretCodeEnter = (event: KeyboardEvent) => {
+		switch (event.key) {
 		case 'Enter':
 			return setShowSecret(secretCode.current === process.env.NEXT_PUBLIC_UNLOCK_SECRET_PASSWORD)
 		case 'Backspace':
@@ -43,7 +27,7 @@ const GalleryCategoriesList: FC = () => {
 		case 'Shift':
 			break
 		default:
-			secretCode.current += e.key
+			secretCode.current += event.key
 		}
 	}
 
@@ -52,18 +36,18 @@ const GalleryCategoriesList: FC = () => {
 
 		return () => document.body.removeEventListener('keydown', secretCodeEnter)
 	}, [])
-	
-	if (!categories) {
+
+	if (isLoading) {
 		return <Center><Loader/></Center>
 	}
 
-	if (errorMessage) {
-		return <ErrorMessage message={errorMessage}/>
+	if (error) {
+		return <ErrorMessage message={errorMessage(error)}/>
 	}
 
 	return (
 		<div className={styles.wrapper}>
-			{categories.map(category => {
+			{categories && categories.map((category) => {
 				if (!showSecret && category.name === process.env.NEXT_PUBLIC_SECRET_CATEGORY) {
 					return null
 				}
