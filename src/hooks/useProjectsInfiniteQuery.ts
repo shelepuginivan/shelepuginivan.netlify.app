23cab2992278d2366236b58031f7fd50d9c28f96
shelepuginivan <shelepuginivanm@gmail.com>
2023-06-26 19:39:07 +0300

    feat: add custom hook `useProjectsInfiniteQuery`

---
+++ src/hooks/useProjectsInfiniteQuery.ts
@@ -0,0 +1,10 @@
+import {useInfiniteQuery} from '@tanstack/react-query'
+
+import {fetchProjects} from '@/api/projects'
+
+export const useProjectsInfiniteQuery = (imagesPerPage = 10) => useInfiniteQuery({
+	queryFn: ({ pageParam = 1 }) => fetchProjects(pageParam, imagesPerPage),
+	queryKey: ['projects'],
+	getNextPageParam: (lastPage, allPages) =>
+		lastPage.length ? allPages.length + 1 : undefined
+})
