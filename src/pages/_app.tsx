aa4d69e18db90d960e73613ee5aeaaaf453a20fe
shelepuginivan <shelepuginivanm@gmail.com>
2023-06-26 19:33:41 +0300

    feat: wrap app in `QueryClientProvider`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,15 +4,21 @@ import '@/styles/variables.sass'
 import '@/styles/media.sass'
 import '@/assets/fonts/icomoon/style.css'
 
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import type { AppProps } from 'next/app'
+import {useState} from 'react'
 
 import Footer from '@/components/Footer/Footer'
 import Header from '@/components/Header/Header'
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <>
-		<Header/>
-		<Component {...pageProps} />
-		<Footer/>
-	</> 
+	const [queryClient] = useState(() => new QueryClient())
+
+	return (
+		<QueryClientProvider client={queryClient}>
+			<Header/>
+			<Component {...pageProps} />
+			<Footer/>
+		</QueryClientProvider>
+	)
 }
