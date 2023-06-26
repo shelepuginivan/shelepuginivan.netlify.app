9b1b280bea91e5a8eaf1810706f4466eeec374d4
shelepuginivan <shelepuginivanm@gmail.com>
2023-06-26 19:31:53 +0300

    feat: add blog api requests

---
+++ src/api/blog.ts
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+import {getHost} from '@/utils/getHost'
+import {Article} from '@/utils/types/Article'
+
+export const fetchArticleBySlug = async (slug: string): Promise<Article> => {
+	const res = await axios.get(`${getHost()}/api/blog/${slug}`)
+	return res.data
+}
+
+export const fetchBlogArticles = async (page: number, articlesPerPage: number): Promise<Omit<Article, 'text'>[]> => {
+	const res = await axios.get(`/api/blog?page=${page}&articlesPerPage=${articlesPerPage}`)
+	return res.data
+}
+
