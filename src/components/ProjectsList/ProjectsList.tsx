6d6ce06eb4abd39086c57a6950df9cef842c2f46
shelepuginivan <shelepuginivanm@gmail.com>
2023-06-26 19:39:26 +0300

    feat: use hook `useProjectsInfiniteQuery` to fetch projects with pagination

--- src/components/ProjectsList/ProjectsList.tsx
+++ src/components/ProjectsList/ProjectsList.tsx
@@ -1,69 +1,39 @@
-import {FC, useCallback, useEffect, useState} from 'react'
+import {FC} from 'react'
 import InfiniteScroll from 'react-infinite-scroll-component'
 
 import ProjectCard from '@/components/ProjectCard/ProjectCard'
+import {useProjectsInfiniteQuery} from '@/hooks/useProjectsInfiniteQuery'
 import Center from '@/ui/Center/Center'
 import ErrorMessage from '@/ui/ErrorMessage/ErrorMessage'
 import Loader from '@/ui/Loader/Loader'
-import {Project} from '@/utils/types/Project'
+import {errorMessage} from '@/utils/errorMessage'
 
 import styles from './projectsList.module.sass'
 
 const ProjectsList: FC = () => {
-	const [currentPage, setCurrentPage] = useState(1)
-	const [hasMore, setHasMore] = useState(false)
-	const [projects, setProjects] = useState<Project[] | null>(null)
-	const [errorMessage, setErrorMessage] = useState<string | null>(null)
-
-	const fetchProjects = useCallback(async () => {
-		const res = await fetch(`/api/projects?page=${currentPage}`)
-		const json = await res.json()
-
-		if (res.status >= 400) {
-			return setErrorMessage((json as Record<'message', string>).message)
-		}
-
-		if (json.length === 0) {
-			return setHasMore(false)
-		}
-
-		setProjects(prev => prev ? [...prev, ...json] : json)
-		setCurrentPage(prev => prev + 1)
-	}, [currentPage])
-
-	useEffect(() => {
-		fetchProjects()
-	}, [])
-
-	if (!projects) {
-		return <Center><Loader/></Center>
+	const { data, error, fetchNextPage, hasNextPage, isLoading } = useProjectsInfiniteQuery()
+	
+	if (error) {
+		return <ErrorMessage message={errorMessage(error)} />
 	}
-
-	if (errorMessage) {
-		return <ErrorMessage message={errorMessage ?? 'Не удалось загрузить проекты'} />
+	
+	if (isLoading) {
+		return <Center><Loader/></Center>
 	}
 
 	return (
 		<InfiniteScroll
 			className={styles.list}
-			next={fetchProjects}
+			next={fetchNextPage}
 			loader={<Loader/>}
-			hasMore={hasMore}
-			dataLength={projects.length}
+			hasMore={Boolean(hasNextPage)}
+			dataLength={data?.pages.length ?? 0}
 		>
-			{
-				projects.map((project, index) => (
-					<ProjectCard
-						key={index}
-						title={project.title}
-						description={project.description}
-						previewImage={project.previewUrl}
-						githubLink={project.githubLink}
-						demoLink={project.demoLink}
-						badges={project.badgesUrls}
-					/>
-				))
-			}
+			{data?.pages.map(projects =>
+				projects.map(project =>
+					<ProjectCard key={project.title} {...project}/>
+				)
+			)}
 		</InfiniteScroll>
 	)
 }
