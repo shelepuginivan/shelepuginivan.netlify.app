71ddfe29e25c7362e13ae042af2bcc4e4d366571
shelepuginivan <shelepuginivanm@gmail.com>
2023-06-26 19:31:59 +0300

    feat: add gallery api requests

---
+++ src/api/gallery.ts
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+import {GalleryCategory} from '@/utils/types/GalleryCategory'
+
+export const fetchGalleryCategories = async (): Promise<GalleryCategory[]> => {
+	const res = await axios.get('/api/gallery')
+	return res.data
+}
+
+export const fetchGalleryImages = async (
+	category: string,
+	page: number,
+	imagesPerPage: number
+): Promise<string[]> => {
+	const res = await axios.get(`/api/gallery/${category}?page=${page}&imagesPerPage=${imagesPerPage}`)
+	return res.data
+}
