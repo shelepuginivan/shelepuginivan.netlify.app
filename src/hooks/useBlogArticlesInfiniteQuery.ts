1892f8a175fd3c9574950c5173cc351f74a5f255
shelepuginivan <shelepuginivanm@gmail.com>
2023-06-26 19:38:24 +0300

    feat: add custom hook `useBlogArticlesInfiniteQuery`

---
+++ src/hooks/useBlogArticlesInfiniteQuery.ts
@@ -0,0 +1,10 @@
+import {useInfiniteQuery} from '@tanstack/react-query'
+
+import {fetchBlogArticles} from '@/api/blog'
+
+export const useBlogArticlesInfiniteQuery = (articlesPerPage = 10) => useInfiniteQuery({
+	queryFn: ({ pageParam = 1 }) => fetchBlogArticles(pageParam, articlesPerPage),
+	queryKey: ['blog-articles'],
+	getNextPageParam: (lastPage, allPages) =>
+		lastPage.length ? allPages.length + 1 : undefined
+})
