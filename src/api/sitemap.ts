354657e12f794d31aed01025dfaec9ad56c7a767
shelepuginivan <shelepuginivanm@gmail.com>
2023-06-26 19:32:13 +0300

    feat: add sitemap api requests

---
+++ src/api/sitemap.ts
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+import {getHost} from '@/utils/getHost'
+import {GalleryCategory} from '@/utils/types/GalleryCategory'
+
+export const fetchAllGalleryCategories = async (): Promise<GalleryCategory[]> => {
+	const res = await axios.get(`${getHost()}/api/gallery`)
+	return res.data
+}
+
+export const fetchAllSlugs = async (): Promise<string[]> => {
+	const res = await axios.get(`${getHost()}/api/blog/slugs`)
+	return res.data
+}
