017355ef72a9812310f4aeffebb5df9cb5894ecd
shelepuginivan <shelepuginivanm@gmail.com>
2023-06-27 22:13:59 +0300

    feat: use hook `useSubmitFeedbackMutation` for feedback submission

--- src/components/FeedbackForm/FeedbackForm.tsx
+++ src/components/FeedbackForm/FeedbackForm.tsx
@@ -1,17 +1,15 @@
-import { Nunito, Roboto_Flex } from 'next/font/google'
-import { FC, FormEvent, useState } from 'react'
+import { Roboto_Flex } from 'next/font/google'
+import { FC, FormEvent } from 'react'
 
+import FeedbackFormHeader from '@/components/FeedbackFormHeader/FeedbackFormHeader'
+import { useSubmitFeedbackMutation } from '@/hooks/useSubmitFeedbackMutation'
 import Button from '@/ui/Button/Button'
 import Input from '@/ui/Input/Input'
 import TextArea from '@/ui/TextArea/TextArea'
 import { EMAIL_MAX_LENGTH, FEEDBACK_MAX_LENGTH, FIRSTNAME_MAX_LENGTH, LASTNAME_MAX_LENGTH } from '@/utils/constants'
+import { Feedback } from '@/utils/types/Feedback'
 
 import styles from './feedbackForm.module.sass'
-
-const nunito = Nunito({
-	subsets: ['cyrillic'],
-	weight: '300'
-})
 
 const robotoFlex = Roboto_Flex({
 	subsets: ['cyrillic', 'latin'],
@@ -19,61 +17,29 @@ const robotoFlex = Roboto_Flex({
 })
 
 const FeedbackForm: FC = () => {
-	const [submitted, setSubmitted] = useState<boolean | null>(null)
-	const [header, setHeader] = useState<string>('Форма обратной связи')
-	const [message, setMessage] = useState<string>('')
+	const { mutate: submitFeedback, error, status } = useSubmitFeedbackMutation()
 
-	const submitFeedback = async (e: FormEvent) => {
-		e.preventDefault()
-
-		type FormField = 'firstname' | 'lastname' | 'email' | 'feedback'
-
-		const form = e.target as unknown as Record<FormField, HTMLInputElement>
-
-		const data: Record<FormField, string> = {
+	const onSubmit = async (event: FormEvent) => {
+		event.preventDefault()
+		
+		const form = event.target as unknown as Record<keyof Feedback, HTMLInputElement>
+		
+		const feedback: Feedback = {
 			firstname: form.firstname.value,
 			lastname: form.lastname.value,
 			email: form.email.value,
-			feedback: form.feedback.value
+			message: form.message.value
 		}
-
-		const res = await fetch('/api/feedback', {
-			method: 'POST',
-			body: JSON.stringify(data),
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		})
-
-		const status = res.status
-
-		setSubmitted(status < 400)
-		setHeader(status < 400 ? 'Отправлено!' : 'Ошибка!')
-
-		if (status >= 400) {
-			try {
-				const json = await res.json()
-				const message = json.message
-				setMessage(message ?? '')
-			} catch (e) {
-				if (e instanceof Error) {
-					setMessage(e.message)
-				}
-			}
-		}
+		
+		await submitFeedback(feedback)
 	}
 
 	return (
-		<div className={styles.formWrapper}>
-			<h1
-				className={nunito.className}
-				data-submitted={submitted}
-			>{header}</h1>
-			<p className={nunito.className} data-error={submitted === false}>{message}</p>
-
+		<div className='container'>
+			<FeedbackFormHeader error={error} status={status}/>
 			<form
+				onSubmit={onSubmit}
 				className={`${styles.form} ${robotoFlex.className}`}
-				onSubmit={submitFeedback}
 			>
 				<Input
 					className={robotoFlex.className}
@@ -101,15 +67,14 @@ const FeedbackForm: FC = () => {
 				/>
 				<TextArea
 					className={robotoFlex.className}
-					id='feedback'
-					name='feedback'
+					id='message'
+					name='message'
 					placeholder={`Введите ваше сообщение (до ${FEEDBACK_MAX_LENGTH} символов)`}
 					maxLength={FEEDBACK_MAX_LENGTH}
 				/>
 				<Button type='submit'>Отправить</Button>
 			</form>
 		</div>
-
 	)
 }
 
