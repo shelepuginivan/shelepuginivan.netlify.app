7c6a5f9308229846f22071f6282c08c1e4c93bef
shelepuginivan <shelepuginivanm@gmail.com>
2023-06-27 22:52:18 +0300

    refactor: remove unused function `isArticleData`

--- src/utils/types/Article.ts
+++ src/utils/types/Article.ts
@@ -5,13 +5,3 @@ export type Article = {
 	slug: string
 	publicationTime: number
 }
-
-export const isArticleData = (article: unknown): article is Omit<Article, 'text'> => {
-	return (
-		typeof article === 'object' &&
-		typeof (article as Record<'title', unknown>).title === 'string' &&
-		typeof (article as Record<'previewUrl', unknown>).previewUrl === 'string' &&
-		typeof (article as Record<'slug', unknown>).slug === 'string' &&
-		typeof (article as Record<'publicationTime', unknown>).publicationTime === 'number'
-	)
-}
